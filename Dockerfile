f22d45cfa38b54fa29e1b61798be941bb444727d
iaean <iaean@users.noreply.github.com>
2022-10-25 13:50:10 +0200

    Initial feed

---
+++ Dockerfile
@@ -0,0 +1,48 @@
+FROM nginx:1.23-alpine
+
+LABEL org.opencontainers.image.title="dns3l NGINX ingress"
+LABEL org.opencontainers.image.description="A docker compose ingress for DNS3L"
+LABEL org.opencontainers.image.version=0.0.0
+
+ENV VERSION=0.0.0
+
+ENV PAGER=less
+
+ARG http_proxy
+ARG https_proxy
+ARG no_proxy
+
+# provided via BuildKit
+ARG TARGETPLATFORM
+ARG TARGETOS
+ARG TARGETARCH
+ARG TARGETVARIANT
+
+# defaults for none BuildKit
+ENV _platform=${TARGETPLATFORM:-linux/amd64}
+ENV _os=${TARGETOS:-linux}
+ENV _arch=${TARGETARCH:-amd64}
+ENV _variant=${TARGETVARIANT:-}
+
+# coreutils setup timeout and fixing a bug in wait-for-it.sh
+# with BusyBox timeout
+RUN apk --update upgrade && \
+    apk add --no-cache coreutils \
+        ca-certificates curl less bash busybox-extras \
+        sudo jq wget dcron openssl bind-tools joe
+
+# Install dockerize
+#
+ENV DCKRZ_VERSION="0.16.3"
+RUN _arch=${_arch/amd64/x86_64} && curl -fsSL https://github.com/powerman/dockerize/releases/download/v$DCKRZ_VERSION/dockerize-${_os}-${_arch}${_variant} > /dckrz && \
+    chmod a+x /dckrz
+
+RUN curl -fsSL https://ssl-config.mozilla.org/ffdhe2048.txt > /etc/nginx/dhparam
+
+COPY nginx.conf /etc/nginx/nginx.tmpl
+COPY nginx.renew.sh /etc/periodic/daily/nginx-renew
+COPY docker-entrypoint.sh /entrypoint.sh
+
+EXPOSE 80 443
+ENTRYPOINT ["/entrypoint.sh"]
+CMD ["nginx", "-g", "daemon off;"]
---
+++ nginx.renew.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+function random_token() {
+  tr -cd '[:alnum:]' </dev/urandom | fold -w32 | head -n1
+}
+
+DNS3L_FQDN=${DNS3L_FQDN:-localhost}
+DNS3L_AUTH_URL=${DNS3L_AUTH_URL:-"https://auth:5554/auth"}
+DNS3L_DAEMON_URL=${DNS3L_DAEMON_URL:-"http://dns3ld:8880/api"}
+CERT_URL=${DNS3L_DAEMON_URL}/ca/les/crt/${DNS3L_FQDN}
+
+SRV_HOST=${SRV_HOST:-ingress}
+SRV_PORT=${SRV_PORT:-443}
+
+# grab SSL TTL from our running service
+SRV_TTL=`echo | openssl s_client -servername ${SRV_HOST} -connect ${SRV_HOST}:${SRV_PORT} 2>/dev/null |\
+  openssl x509 -noout -dates 2>/dev/null | grep ^notAfter | cut -d= -f2`
+
+CLIENT_ID=${CLIENT_ID:-"dns3l-api"}
+CLIENT_SECRET=${CLIENT_SECRET:-$(random_token)}
+DNS3L_USER=${DNS3L_USER:-certbot}
+DNS3L_PASS=${DNS3L_PASS:-$(random_token)}
+
+ID_TOKEN=`curl -k -s -X POST -u "${CLIENT_ID}:${CLIENT_SECRET}" \
+  -d "grant_type=password&scope=openid profile email groups offline_access&username=${DNS3L_USER}&password=${DNS3L_PASS}" \
+  ${DNS3L_AUTH_URL}/token | jq -r .id_token`
+
+if [[ -z ${ID_TOKEN} || ${ID_TOKEN} == "null" ]]; then
+  echo Oooops. Invalid token.
+  exit
+fi
+
+# grab SSL TTL from DNS3L for our service
+DNS3L_TTL=`curl -k -s -H "Authorization: Bearer ${ID_TOKEN}" ${CERT_URL}/pem/crt |\
+  openssl x509 -noout -dates 2>/dev/null | grep ^notAfter | cut -d= -f2`
+
+echo -n >/tmp/nginx.renew.log
+
+# compare...
+if [ -z "$DNS3L_TTL" -o -z "$SRV_TTL" ]; then
+  echo "Error fetching certs from ${SRV_HOST}:${SRV_PORT} or ${CERT_URL}" >>/tmp/nginx.renew.log
+
+  exit 2
+elif [ "$DNS3L_TTL" != "$SRV_TTL" ]; then
+  # TODO: use real integer comparison (>) instead of equal strings...
+  echo "Installing renewed cert..." >>/tmp/nginx.renew.log
+  echo "DNS3L TTL: $DNS3L_TTL" >>/tmp/nginx.renew.log
+  echo "Service TTL: $SRV_TTL" >>/tmp/nginx.renew.log
+
+  # install the renewed cert into our service
+  # REVIEW: dangerous... in case of curl failure.
+  curl -k -s -H "Authorization: Bearer ${ID_TOKEN}" ${CERT_URL}/pem/key > /etc/nginx/privkey.pem
+  curl -k -s -H "Authorization: Bearer ${ID_TOKEN}" ${CERT_URL}/pem/fullchain > /etc/nginx/fullchain.pem
+
+  # restart/reload our service...
+  kill -s SIGHUP 1
+
+  exit 1
+else
+  echo "Nothing to do..." >>/tmp/nginx.renew.log
+  echo "DNS3L TTL: $DNS3L_TTL" >>/tmp/nginx.renew.log
+  echo "Service TTL: $SRV_TTL" >>/tmp/nginx.renew.log
+
+  exit 0
+fi
