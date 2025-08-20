c4ad69d2fddcce3ce68fb5dcd9b90ea8a42ef237
Leonhard Nobach <leonhard.nobach@telekom.de>
2025-08-20 15:04:44 +0200

    Removed dex bootstrap workaround, added access token support

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -23,17 +23,6 @@ function file_env() {
   unset "$fileVar"
 }
 
-# envs=(
-#   XYZ_API_TOKEN
-# )
-# haveConfig=
-# for e in "${envs[@]}"; do
-#   file_env "$e"
-#   if [ -z "$haveConfig" ] && [ -n "${!e}" ]; then
-#     haveConfig=1
-#   fi
-# done
-
 # return true if specified directory is empty
 function directory_empty() {
   [ -n "$(find "${1}"/ -prune -empty)" ]
@@ -82,19 +71,18 @@ fi
 export DNS3L_FQDN=${DNS3L_FQDN:-localhost}
 
 export DNS3L_APP_URL=${DNS3L_APP_URL:-"http://web:3000"}
-export DNS3L_AUTH_URL=${DNS3L_AUTH_URL:-"https://auth1:5554/auth"}
+export DNS3L_AUTH_URL=${DNS3L_AUTH_URL:-"https://auth:5554/auth"}
 export DNS3L_DAEMON_URL=${DNS3L_DAEMON_URL:-"http://dns3ld:8880/api"}
-
 export DNS3L_APP_TIMEOUT=${DNS3L_APP_TIMEOUT:-1m}
 export DNS3L_API_TIMEOUT=${DNS3L_API_TIMEOUT:-6m}
 export DNS3L_AUTH_TIMEOUT=${DNS3L_AUTH_TIMEOUT:-2m}
 
+: "${DNS3L_TOKEN:?not set or empty}"
+
 # Review dead lock behavior once authn is enabled in dns3ld
 #   https://github.com/dns3l/ingress/issues/3
 #   https://github.com/dns3l/dns3l-core/issues/19
-# Workaround to allow another dns3ld that just provides the cert for the ingress
 export DNS3L_BOOT_DAEMON_URL=${DNS3L_BOOT_DAEMON_URL:-"http://dns3ld:8880/api"}
-export DNS3L_BOOT_AUTH_URL=${DNS3L_BOOT_AUTH_URL:-"https://auth:5554/auth"}
 
 if [ -r /etc/nginx.conf -a -s /etc/nginx.conf ]; then
   ln -fs /etc/nginx.conf /etc/nginx/nginx.conf
@@ -106,9 +94,6 @@ else
   #   https://github.com/dns3l/ingress/issues/3
   #   https://github.com/dns3l/dns3l-core/issues/19
   /dckrz -wait ${DNS3L_BOOT_DAEMON_URL}/info -skip-tls-verify -timeout ${SERVICE_TIMEOUT} -- echo "Ok. DNS3L bootstrap daemon is there."
-  /dckrz -wait ${DNS3L_BOOT_AUTH_URL}/.well-known/openid-configuration -skip-tls-verify -timeout ${SERVICE_TIMEOUT} -- echo "Ok. DexIDP bootstrap is there."
-  #/dckrz -wait ${DNS3L_DAEMON_URL}/info -skip-tls-verify -timeout ${SERVICE_TIMEOUT} -- echo "Ok. DNS3L daemon is there."
-  /dckrz -wait ${DNS3L_AUTH_URL}/.well-known/openid-configuration -skip-tls-verify -timeout ${SERVICE_TIMEOUT} -- echo "Ok. DexIDP is there."
 fi
 
 ###
@@ -119,30 +104,17 @@ DNS3L_FQDN_CA=${DNS3L_FQDN_CA:-les}
 read -a fqdns <<< $DNS3L_FQDN
 CERT_NAME=${fqdns[0]} # first FQDN is the cert name
 CERT_URL=${DNS3L_BOOT_DAEMON_URL}/ca/${DNS3L_FQDN_CA}/crt/${CERT_NAME}
-CLIENT_ID=${CLIENT_ID:-"dns3l-api"}
-CLIENT_SECRET=${CLIENT_SECRET:-$(random_token)}
-DNS3L_USER=${DNS3L_USER:-certbot}
-DNS3L_PASS=${DNS3L_PASS:-$(random_token)}
 
 found=1
 
-ID_TOKEN=`curl -k -s -X POST -u "${CLIENT_ID}:${CLIENT_SECRET}" \
-  -d "grant_type=password&scope=openid profile email groups offline_access&username=${DNS3L_USER}&password=${DNS3L_PASS}" \
-  ${DNS3L_BOOT_AUTH_URL}/token | jq -r .id_token`
-
-if [[ -z ${ID_TOKEN} || ${ID_TOKEN} == "null" ]]; then
-  echo Oooops. Invalid token.
-  found=0
-fi
-
 set +e
-curl -k -s -H "Authorization: Bearer ${ID_TOKEN}" ${CERT_URL}/pem/key |\
+curl -k -s -H "X-DNS3L-Access-Token: ${DNS3L_TOKEN}" ${CERT_URL}/pem/key |\
   tee /etc/nginx/privkey.pem | grep -q -- '-----BEGIN .* PRIVATE KEY-----'
 if [[ $? != "0" ]]; then
   echo Oooops. Key ${CERT_URL}/pem/key not found.
   found=0
 fi
-curl -k -s -H "Authorization: Bearer ${ID_TOKEN}" ${CERT_URL}/pem/fullchain |\
+curl -k -s -H "X-DNS3L-Access-Token: ${DNS3L_TOKEN}" ${CERT_URL}/pem/fullchain |\
   tee /etc/nginx/fullchain.pem | grep -q -- '-----BEGIN CERTIFICATE-----'
 if [[ $? != "0" ]]; then
   echo Oooops. Cert ${CERT_URL}/pem/fullchain not found.
